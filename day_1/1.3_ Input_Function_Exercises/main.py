9570c8bc59cddd350fe001e0aaa269a52be4f7e8
sanjayguwaju <sanjayguwaju@gmail.com>
2022-08-23 17:12:40 +0545

    Exercise: It has exercise for input function.

---
+++ day_1/1.3_ Input_Function_Exercises/main.py
@@ -0,0 +1,5 @@
+
+#print is function : Print() it basically prints.
+#len is function: len() is counts lenght of the string.
+#input is function: input() it takes input from the user.
+print(len(input("What is your name ? \n")))
