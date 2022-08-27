08b972a96f1e29eb6586cb088104ecc2cd8d2221
sanjayguwaju <sanjayguwaju@gmail.com>
2022-08-27 07:41:40 +0545

    learned: Learned about variables and how to exchange their values

---
+++ day_1/1.4_Variables_Exercises/main.py
@@ -0,0 +1,23 @@
+#Don't change the code below
+a = input("a:")
+b = input("b:")
+#Don't change the code above
+
+###################################
+#Write your code below this line
+c = a
+#Above we can see new variable c is storing the input value of a (a is empty now)
+a = b
+
+#Above we can see a is storing the value of b ( b is empty now)
+b = c
+
+#Above we can see b is storing the value of c ( c is empty now)
+#Basically we don't care about c variable so we can able to exchange the value of it.
+#Write you code above this line
+
+#Don't change the code below
+print("a = " + a)
+print("b = " + b)
+#Don't change the code above
+
