7101b3df8a9e71960e76086656ee3ad1cbb8b768
sanjayguwaju <sanjayguwaju@gmail.com>
2022-10-11 14:20:07 +0545

    learned: if inside if untill we want to check the codition

--- day_3/3.3-Leap-Year-Exercise/main.py
+++ day_3/3.3-Leap-Year-Exercise/main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+year = int(input("Please enter the year you want to check \n"))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("This is leap year")
+        else:
+            print("This is not a leap year")
+    else:
+        print("This is a leap year")
+else:
+    print("This is not a leap year")
