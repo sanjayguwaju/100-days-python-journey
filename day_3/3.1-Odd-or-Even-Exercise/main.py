477367ca00480782f61c67d09cf32a0b47093d64
sanjayguwaju <sanjayguwaju@gmail.com>
2022-10-11 13:12:09 +0545

    learned: if and else with modulus usage it is odd and even differenciator

---
+++ day_3/3.1-Odd-or-Even-Exercise/main.py
@@ -0,0 +1,10 @@
+
+#Don't change the code below
+number = int(input("Which number do you want to change? \n"))
+#Don't change the code above
+
+#Write your code below this line
+if number % 2 == 0:
+    print("This is even number")
+else:
+    print("This is odd number")
